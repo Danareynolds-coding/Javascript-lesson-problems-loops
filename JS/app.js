3bb59330797db8187cdac61093fc43104d060e07
Danareynolds-coding <dana.reynolds@mscoding.org>
2025-04-22 18:26:21 -0500

    javascript Problems

---
+++ JS/app.js
@@ -0,0 +1,15 @@
+// problem1
+for (let i=0; i<10; i++) {
+    console.log(`(for) i=${i}`);
+}
+//Problem2 
+let names = ["Joseph", "Aaron", "Matthew", "Paul", "David"];
+for (let i=0; i<names.length; i++){
+    console.log("Hello," +names[i]+ "!");
+}
+// Problem3
+for (let i=0; i<100; i++){
+    if (i % 7==0){
+        console.log(`(for)i=${i}`);}
+    }
+
